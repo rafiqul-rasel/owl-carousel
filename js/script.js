d0a75238ae1bcda768f2d54a5c50a3ba8ff84b60
Md.Rafiqul Islam <rafiqul.islam@brainstation-23.com>
2022-01-09 20:54:09 +0600

    owl-carousel customize

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+jQuery( document ).ready(function() {
+    $('.owl-carousel').owlCarousel({
+    	items:1,
+    	loop:true,
+    	nav:true,
+    	dots:true,
+    	autoplay:true,
+    	autoplayspeed:1000,
+    	smartSpeed:1500,
+    	autoplayHoverPause:true
+
+    });
+});
